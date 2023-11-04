baba6ff45ff7142979ca0230303a40416c2b5f9a
jumaanr <jumaanr@outlook.com>
2023-11-04 21:10:08 +0530

    added gcp_init.sh

---
+++ gcp_init.sh
@@ -0,0 +1,9 @@
+#Enable or disable Accessibility Feature : https://cloud.google.com/sdk/docs/enabling-accessibility-features
+gcloud config set accessibility/screen_reader true
+
+gcloud init # will initialize the configuration of gcloud
+gcloud init --skip-diagnostics # skip diagnostics next time
+gcloud --help #see cloud platform services you can interact with
+gcloud topic --help #learn about advanced features
+gcloud cheat sheet #see a roster of go-to glcloud commands
+
