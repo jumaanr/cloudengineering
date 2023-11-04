85073892d5074081abfb4a735e9893dc9e484ecd
jumaanr <jumaanr@outlook.com>
2023-11-04 21:20:00 +0530

    added packages directory

--- gcp_init.sh
+++ gcp_init.sh
@@ -1,3 +1,10 @@
+#!/bin/bash
+
+#CLI Installation
+curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-453.0.0-linux-x86_64.tar.gz
+tar -xf <Package_Name>
+#
+
 #Enable or disable Accessibility Feature : https://cloud.google.com/sdk/docs/enabling-accessibility-features
 gcloud config set accessibility/screen_reader true
 
