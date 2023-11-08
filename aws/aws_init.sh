3c5576cdbfcfc73cb5a0851d662d4c467444456a
jumaanr <jumaanr@outlook.com>
2023-11-08 17:50:22 +0530

    added aws keys and connected with VM

--- aws/aws_init.sh
+++ aws/aws_init.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-#This is to pratcie AWS Cli
-#root account aws_admin https://179666141320.signin.aws.amazon.com/console
+# This is to pratcie AWS Cli
+# IAM admin account:  aws_admin, https://179666141320.signin.aws.amazon.com/console
+# bastion : awsstation01
+# private key has been stored
 
+#connect with AWS account
+aws configure
+#Provide access keys and login 
+#---------- Connecting with EC2 instance -------
+chmod 400 awsstation01.pem #giving necessary permissions 
+ssh -i "./keys/awsstation01.pem" ec2-user@ec2-54-158-23-13.compute-1.amazonaws.com #connect with DNS name
