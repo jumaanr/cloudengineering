fec02b873166c3c954024be373d127356041701b
jumaanr <jumaanr@outlook.com>
2024-02-13 10:48:39 +0400

    Terraform Notes have been added

---
+++ terraform/terraforminit.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Install Terraform
+
+# download terrafrom : https://developer.hashicorp.com/terraform/install?product_intent=terraform
+
+#RHEL
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
+sudo yum -y install terraform
+
+#Debian/Ubuntu
+wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update && sudo apt install terraform
