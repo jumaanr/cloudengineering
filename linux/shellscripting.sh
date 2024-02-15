dddd5595310598b5248ebe4f894eb27d42763de3
jumaanr <jumaanr@outlook.com>
2024-02-15 07:06:25 +0400

    added shellscript content

---
+++ linux/shellscripting.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Running a shell script--------------------------------------------------------------------
+bash create-and-launch-rocket.sh  # shell scripts executes line by line
+sh create-and-launch-rocket.sh
+
+# Defining shell script as a command
+
+$ create-and-launch-rocket # how do we do this ?
+# Note: Include this script to $PATH variable  by appending the script to the variable. And also remove .sh extension
+$ export PATH=$PATH:/home/michael
+
+
+#Variables
+
+#Control Logic
+
+#Loops
+
+#Executing a Shell Script
+
+# Arithmetic Operations
+
+# Best Practices/IDEs
+
