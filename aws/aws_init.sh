72a5faa52bd92e912c88abacfcc9937e9336a9f0
jumaanr <jumaanr@outlook.com>
2024-06-26 15:12:19 +0530

    Started AWS Solutions Architect Associate Work Project

--- aws/aws_init.sh
+++ aws/aws_init.sh
@@ -1,13 +1,1 @@
-#!/bin/bash
-
-# This is to pratcie AWS Cli
-# IAM admin account:  aws_admin, https://179666141320.signin.aws.amazon.com/console
-# bastion : awsstation01
-# private key has been stored
-
-#connect with AWS account
-aws configure
-#Provide access keys and login 
-#---------- Connecting with EC2 instance -------
-chmod 400 awsstation01.pem #giving necessary permissions 
-ssh -i "./keys/awsstation01.pem" ec2-user@ec2-54-158-23-13.compute-1.amazonaws.com #connect with DNS name
+#This is an interface that would connect to AWS CLI
